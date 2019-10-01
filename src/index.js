faff81ccbeb08eab754c062216cc21f3d03de11d
RoGueFroM <roguefrombehind@gmail.com>
2019-10-02 00:55:57 +0300

    Start of Event practice

--- src/index.js
+++ src/index.js
@@ -1,57 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
-import {Lesson, Counter, Button} from "./Lesson PropTypes";
+import App from "./Events/Events.js";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// PropTypes
 
-// // Node:
-// Component.propTypes = {
-// 	node: PropTypes.node
-// }
-
-// // Element:
-// Component.propTypes = {
-// 	elem: PropTypes.element
-// }
-
-// // Instance of:
-// Component.propTypes = {
-// 	instance: PropTypes.instanceOf(Constructor)
-// }
-
-// // One of:
-// Component.propTypes = {
-// 	elem: PropTypes.oneOf(["var1", "var2"])
-// }
-
-// // One of type:
-// Component.propTypes = {
-// 	elem: PropTypes.oneOfType([
-// 		PropTypes.string, 
-// 		PropTypes.number
-// 	])
-// }
-
-// // Array of / Object of:
-// Component.propTypes = {
-// 	array: PropTypes.arrayOf(PropTypes.string),
-// 	object: PropTypes.objectOf(PropTypes.number)		
-// }
-
-// // Shape:
-// Component.propTypes = {
-// 	obj: PropTypes.shape({
-// 		color: PropTypes.string,
-// 		fontSize: PropTypes.number,
-// 		lineHeight:	PropTypes.number
-// 	})
-// }
-
-// ====================================================================================
-
-ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
