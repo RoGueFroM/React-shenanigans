9227ed9825050b22fdc39a0336c4d983f8e50d0d
RoGueFroM <roguefrombehind@gmail.com>
2019-10-03 00:31:09 +0300

    Ternary conditional rendering

--- src/Conditional rendering/Conditional rendering.js
+++ src/Conditional rendering/Conditional rendering.js
@@ -1,17 +1,14 @@
 import React, {Component} from "react";
 
-const ValidationMsg = ({ val }) => {
-	if (val >= 10) {
-		return <h2>Grate than 10</h2>
-	}
-	else {
-		return <h3>Less than <em>10</em></h3>
-	}
-}
+const ValidationMsg = ({ val }) => {}
 
 const App = () => {
+	const val = 9;
 	return (
-		<ValidationMsg val={8} />
+		<div>
+			{val >= 10 ? <h2>Grate than 10</h2> : <h3>Less than <em>10</em></h3>}
+			{val && <span>We have {val}</span>}
+		</div>
 	);
 }
 
