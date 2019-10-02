9df5a29d958b82f2007383c786d4c323c8a63f37
RoGueFroM <roguefrombehind@gmail.com>
2019-10-03 02:44:02 +0300

    List and keys usage, rendering using configuration list TABS_BTN

--- src/Conditional rendering/Conditional rendering.js
+++ src/Conditional rendering/Conditional rendering.js
@@ -12,6 +12,25 @@ const Tab3 = () => (
 	<h1>Text of tab3</h1>
 )
 
+const people = ["Jack", "Max", "Leo", "Alex"];
+
+const TABS_BTN = [
+	{
+		dataName: 1,
+		title: "Tab1",
+		icon: "+"
+	},
+	{
+		dataName: 2,
+		title: "Tab2",
+		icon: "-"
+	},
+	{
+		dataName: 3,
+		title: "Tab3",
+		icon: "*"
+	},
+];
 
 class App extends Component {
 	state = {
@@ -28,15 +47,22 @@ class App extends Component {
 		const { activeTab } = this.state;
 		return (
 		<Fragment>
-			<button data-name={1} onClick={this.handleTab}>Tab1</button>
-			<button data-name={2} onClick={this.handleTab}>Tab2</button>
-			<button data-name={3} onClick={this.handleTab}>Tab3</button>
+			{TABS_BTN.map( ({dataName, title, icon}) => (
+				<button 
+				key={`${dataName}-${title}`} 
+				data-name={dataName} 
+				onClick={this.handleTab}
+				>{icon}{title}</button>
+			))}
 			{activeTab === 1 && <Tab1/>}
 			{activeTab === 2 && <Tab2/>}
 			{activeTab === 3 && <Tab3/>}
 			<div>
 				Active tab is: {activeTab === 1 ? "first" : activeTab === 2 ? "second" : "third"}
 			</div>
+			<ul>
+				{people.map( (person, index) => ( <li key={index}>{person}</li> ) )}
+			</ul>
 		</Fragment>
 		);
 	}
