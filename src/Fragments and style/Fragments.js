974f8fc3f1ce7a83c633fca7629e5e53266f6eb6
RoGueFroM <roguefrombehind@gmail.com>
2019-10-03 06:02:37 +0300

    Added styles file and bound class to element

--- src/Fragments and style/Fragments.js
+++ src/Fragments and style/Fragments.js
@@ -1,15 +1,17 @@
 import React, {Component, Fragment} from "react";
+import './styles.css'; //import style file here!!!!!!!!!
 
 // const Columns = () => ([
 // 		<td key="1">Hello</td>,
 // 		<td key="2">World</td>
 // ]);
 
+const styles = {color: "red", textTransform: "uppercase"};
 
 const Columns = () => (
 		<Fragment>
-			<td style={{color: "red", textTransform: "uppercase"}}>Inline</td>
-			<td>Simple Class</td>
+			<td style={styles}>Inline</td>
+			<td className="title">Simple Class</td>
 		</Fragment>
 );
 
