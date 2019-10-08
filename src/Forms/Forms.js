da46273dd7623790c0517d8b55821aac3f2221f9
RoGueFroM <roguefrombehind@gmail.com>
2019-10-08 11:41:34 +0300

    Started form creating

--- src/Forms/Forms.js
+++ src/Forms/Forms.js
@@ -1,17 +1,35 @@
 import React, {Component} from "react";
 
 class Form extends Component {
+	state = {
+		inputText: "",
+		textareaText: ''
+	}
+
+	handleInputChange = ({ target: {value}}) => {
+		this.setState({
+			inputText: value,
+		})
+	}
+
+	handleTextareaChange = ({ target: {value}}) => {
+		this.setState({
+			textareaText: value,
+		})
+	}
+
 	render() {
+		const {inputText, textareaText} = this.state;
 		return (
 			<form>
 				{/*Input*/}
 				<label>
 					Name:
-					<input type="text" />
+					<input type="text" name="name" value={inputText} onChange={this.handleInputChange}/>
 				</label> 
 				{/*Text area*/} <br />
-				<label htmlFor="text"></label>
-				<textarea id="text">Text:</textarea>
+				<label htmlFor="text">Text:</label>
+				<textarea id="text" value={textareaText} onChange={this.handleTextareaChange}/>
 				{/*Button*/} <br />
 				<button>Show</button>
 			</form>
