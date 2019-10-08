fd8c663443d2c074063a9c9e6a9332031887eb52
RoGueFroM <roguefrombehind@gmail.com>
2019-10-08 13:21:28 +0300

    Completed complicated form

--- src/Forms/Forms.js
+++ src/Forms/Forms.js
@@ -1,9 +1,27 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
+
+const POSITIONS = [
+	{
+		id: 'fd',
+		value: 'Front-end developer',
+		title: 'Front-end developer'
+	},
+	{
+		id: 'bd',
+		value: 'Back-end developer',
+		title: 'Back-end developer'
+	}
+]
 
 class Form extends Component {
 	state = {
 		inputText: "",
-		textareaText: ''
+		textareaText: '',
+		showData: {
+			name: '',
+			text: '',
+			position: '',
+		}
 	}
 
 	handleInputChange = ({ target: {value}}) => {
@@ -14,25 +32,55 @@ class Form extends Component {
 
 	handleTextareaChange = ({ target: {value}}) => {
 		this.setState({
-			textareaText: value,
+			textareaText: value, 
+		})
+	}
+
+	handleSelectChange = ({ target: {value}}) => {
+		this.setState({
+			selectText: value, 
+		})
+	}
+
+	handleShow = (e) => {
+		e.preventDefault();
+		const {inputText, textareaText, selectText} = this.state;
+		this.setState({
+			inputText: '',
+			textareaText: '',
+			showData: {
+				name: inputText,
+				text: textareaText,
+				position: selectText,
+			}
 		})
 	}
 
 	render() {
-		const {inputText, textareaText} = this.state;
+		const {inputText, textareaText, selectText, showData} = this.state;
+		const {name, text, position} = showData;
 		return (
-			<form>
-				{/*Input*/}
-				<label>
-					Name:
-					<input type="text" name="name" value={inputText} onChange={this.handleInputChange}/>
-				</label> 
-				{/*Text area*/} <br />
-				<label htmlFor="text">Text:</label>
-				<textarea id="text" value={textareaText} onChange={this.handleTextareaChange}/>
-				{/*Button*/} <br />
-				<button>Show</button>
-			</form>
+			<Fragment>
+				<form>
+					{/*Input*/}
+					<label>
+						Name:
+						<input type="text" name="name" value={inputText} onChange={this.handleInputChange}/>
+					</label> 
+					{/*Text area*/} <br />
+					<label htmlFor="text">Text:</label>
+					<textarea id="text" value={textareaText} onChange={this.handleTextareaChange}/>
+					{/*Select*/}
+					<select value={selectText} onChange={this.handleSelectChange}>
+						{POSITIONS.map( ({id, value, title}) => (<option key={id} value={value}>{title}</option>) )}
+					</select>
+					{/*Button*/} <br />
+					<button onClick={this.handleShow}>Show</button>
+				</form>
+				<h2>{name}</h2>
+				<h3>{text}</h3>
+				<h3>{position}</h3>
+			</Fragment>
 		)
 	}
 }
